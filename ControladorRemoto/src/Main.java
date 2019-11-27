b504a53d991b89c2dfd7163f23784f4354ef19fb
aluno <adsroot.aluno.ifsc.edu.br>
2019-11-26 21:34:45 -0300

    correção coordenadas

--- ControladorRemoto/src/Main.java
+++ ControladorRemoto/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 		try {
 			
 			RemoteAccess obj = (RemoteAccess) Naming
-					.lookup("rmi://localhost/RemoteControler");
+					.lookup("rmi://10.61.5.82/RemoteControler");
 			
 			String token = obj.logIn("12345");
 			
@@ -32,7 +32,7 @@ public class Main {
 	
 			byte screenshot[] = obj.getScreenshot(token);
 			
-			ShowScreen s = new ShowScreen(screenshot);
+			ShowScreen s = new ShowScreen(screenshot, obj, token);
 			s.setVisible(true);
 			s.start();
 			for(;;){
@@ -52,14 +52,34 @@ implements MouseListener, MouseMotionListener {
 	/**
 	 * 
 	 */
+	private RemoteAccess obj;
+	private String token ;
+	int altReal, largReal, altJan, largJan;
+	Dimension size;
+	double dif = 0.98;
+	
 	private static final long serialVersionUID = -1842261777470977698L;
 
-	public ShowScreen(byte image[]) {
+	public ShowScreen(byte image[], RemoteAccess obj, String token)  {
+		
 		a = new javax.swing.JLabel();
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		a.setIcon(new javax.swing.ImageIcon(image));
 		add(a);
 		pack();
+		this.obj = obj;
+		altJan = getHeight();
+		largJan=getWidth();
+		this.token = token;
+		size = a.getSize();
+		try {
+			altReal = obj.getHeightResolution(token);
+			largReal = obj.getWidthResolution(token);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	public void update(byte image[]){
 		a.setIcon(new javax.swing.ImageIcon(image));
@@ -68,58 +88,77 @@ implements MouseListener, MouseMotionListener {
 	private javax.swing.JLabel a;
 	
 	
-	JLabel mousePosition;
+	public JLabel mousePosition;
     @Override
     public void mouseClicked(MouseEvent e) {
-        mousePosition.setText("Mouse clicado na coordenada : ["+e.getX()+","+e.getY()+"]");
+       // mousePosition.setText("Mouse clicado na coordenada : ["+e.getX()+","+e.getY()+"]");
           
     }
   
     @Override
     public void mouseEntered(MouseEvent e) {
-        mousePosition.setText("Coordenada atual do mouse : ["+e.getX()+","+e.getY()+"]");
+        //mousePosition.setText("Coordenada atual do mouse : ["+e.getX()+","+e.getY()+"]");
           
     }
   
     @Override
     public void mouseExited(MouseEvent e) {
-        mousePosition.setText("O Mouse está fora da janela de acesso");
+       // mousePosition.setText("O Mouse está fora da janela de acesso");
           
     }
   
     @Override
     public void mousePressed(MouseEvent e) {
-        mousePosition.setText("Mouse pressionado nas coordenadas : ["+e.getX()+","+e.getY()+"]");
+       // mousePosition.setText("Mouse pressionado nas coordenadas : ["+e.getX()+","+e.getY()+"]");
           
     }
   
     @Override
     public void mouseReleased(MouseEvent e) {
-        mousePosition.setText("Coordenada atual do mouse : ["+e.getX()+","+e.getY()+"]");
+       // mousePosition.setText("Coordenada atual do mouse : ["+e.getX()+","+e.getY()+"]");
           
     }
   
     @Override
     public void mouseDragged(MouseEvent e) {
-        mousePosition.setText("Mouse arrastado nas coordenadas : ["+e.getX()+","+e.getY()+"]");
+        //mousePosition.setText("Mouse arrastado nas coordenadas : ["+e.getX()+","+e.getY()+"]");
           
     }
-  
+    public int coordRealAlt(int coordJanela) {
+    	return (int) (((altReal* coordJanela)/altJan)) ;
+    }
+    public int coordRealLarg(int coordJanela) {
+    	return (int) (((largReal* coordJanela)/largJan) ) ;
+    }
     @Override
     public void mouseMoved(MouseEvent e) {
-        mousePosition.setText("Mouse movido para as coordenadas : ["+e.getX()+","+e.getY()+"]");
+        
+        try {
+        	System.out.println("______________________________________");
+        	System.out.println("___ Resul. Real: "+altReal+" X "+largReal+"     ____");
+        	System.out.println("____Resul. janela:   "+altJan+" X "+largJan+"     ____");
+        	System.out.println("____Coordenada Janela:  "+ e.getY() +"  X  "+e.getX()+"  ____");
+        	System.out.println("____Coordenada real:  "+coordRealLarg(e.getX())+" X "+coordRealAlt(e.getY())+"        ____");
+        	System.out.println("______________________________________");
+			obj.moveMouse(token,coordRealLarg(e.getX()), coordRealAlt(e.getY())
+					
+					);
+		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
           
     }
   
     public void start()
     {
-        mousePosition=new JLabel();
+       // mousePosition=new JLabel();
          addMouseListener( this );        // listens for own mouse and
           addMouseMotionListener( this );  // mouse-motion events
-          setLayout(new FlowLayout(1));
-          add(mousePosition);
-          Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-          setSize(size );
+         // setLayout(new FlowLayout(1));
+         // add(mousePosition);
+          size = Toolkit.getDefaultToolkit().getScreenSize();
+          //setSize(size );
           setUndecorated(true);
           setVisible( true );
           setDefaultCloseOperation(EXIT_ON_CLOSE);
